f39242479f1a1077bf220c46510b1958d74faf81
Aryan Sharma <aryan.sharma@nonceblox.com>
2022-03-28 17:51:20 +0530

    feat(projects api): created api

--- src/auth/decorators/roles.guard.ts
+++ src/auth/decorators/roles.guard.ts
@@ -1,19 +1,16 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
 import { Role } from './roles.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
-      process.env.ROLES_KEY,
-      [context.getHandler(), context.getClass()],
-    );
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
 
     if (!requiredRoles) {
       return true;
@@ -21,6 +18,6 @@ export class RolesGuard implements CanActivate {
 
     const { user } = context.switchToHttp().getRequest();
 
-    return requiredRoles.some((role) => user.roles.includes(role));
+    return requiredRoles.some((role) => user.roles?.includes(role));
   }
 }
