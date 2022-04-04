cc0e50d394ed06a1b3a6ac21a0e55284c999b0ac
naveen reddy <naveen.reddy9688@gmail.com>
2022-04-05 00:17:54 +0530

    feat(events config): created dynamic collection

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, Req } from '@nestjs/common';
 import { EventsService } from './events.service';
 
 @Controller('events')
@@ -6,7 +6,9 @@ export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
   @Get('test')
-  test() {
+  test(@Req() req) {
+    const { projectName } = req.query;
+
     const ERC20_ABI = [
       'event Listed(address nft, uint256 nftId, address seller, uint256 price)',
     ];
@@ -14,6 +16,7 @@ export class EventsController {
     return this.eventsService.test(
       '0xD68603215c4646386d2e0bE68a38027CE4a7652d',
       ERC20_ABI,
+      projectName,
     );
   }
 }
