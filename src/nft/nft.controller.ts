7049f31c3953685b0417ff0afeb1b9ca84756cbb
Aryan Sharma <aryan.sharma@nonceblox.com>
2022-04-28 18:28:33 +0530

    fix: import issue

--- src/nft/nft.controller.ts
+++ src/nft/nft.controller.ts
@@ -6,7 +6,8 @@ import {
 } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import constants from 'docs/constants';
-import { GetNFTsDTO } from './dtos/Get-nfts';
+import { GetNFTsDTO } from './dtos/get-nfts';
+
 import { NftService } from './nft.service';
 
 @Controller('nfts')
